5f756ac1a0de7d28844b2119be8153fec3295728
JamesMurrayBIT <jmy9605@163.com>
2017-11-19 21:11:10 +0800

    2017-11-19 submit

---
+++ BluetoothFastConfig/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace BluetoothFastConfig
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "9600";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "串口号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "波特率：";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(80, 98);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(126, 23);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "我不知道波特率";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 164);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口设置";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(198, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 27);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "刷新串口";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(80, 21);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(111, 27);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(315, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(304, 164);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "蓝牙设置";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(107, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 27);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "波特率：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(107, 69);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(167, 26);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "1234";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "配对密码：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(107, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "BITSCC-Groupx";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "模块名字：";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(236, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "一键配置";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(4, 225);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(623, 189);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(471, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "帮助";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(552, 182);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "关于";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 426);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "蓝牙快速设置助手";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ BluetoothFastConfig/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace BluetoothFastConfig
+{
+    public partial class Form1 : Form
+    {
+        int TrueBAUD = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("1200");
+            comboBox1.Items.Add("2400");
+            comboBox1.Items.Add("4800");
+            comboBox1.Items.Add("9600");
+            comboBox1.Items.Add("19200");
+            comboBox1.Items.Add("38400");
+            comboBox1.Items.Add("57600");
+            comboBox1.Items.Add("115200");
+            comboBox1.Items.Add("230400");
+            comboBox1.Items.Add("460800");
+            comboBox1.Items.Add("921600");
+            comboBox1.Items.Add("1382400");
+            comboBox1.SelectedIndex = 3;
+
+            comboBox2.Items.Add("请选择");
+            comboBox2.SelectedIndex = 0;
+            IsPortOpen();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox1.Text = "程序将尝试所有波特率";
+                textBox1.Enabled = false;
+            }
+            else
+            {
+                textBox1.Text = "9600";
+                textBox1.Enabled = true;
+            }
+        }
+        private void L(String s,int color)
+        {
+            if (color == 0) richTextBox1.SelectionColor = Color.Black;
+            if (color == 1) richTextBox1.SelectionColor = Color.Blue;
+            if (color == 2) richTextBox1.SelectionColor = Color.Green;
+            if (color == 3) richTextBox1.SelectionColor = Color.Red;
+            richTextBox1.AppendText("[" + DateTime.Now.ToString("hh:mm:ss") + "] " + s + "\r\n");
+            this.richTextBox1.SelectionStart = this.richTextBox1.TextLength;
+            this.richTextBox1.ScrollToCaret(); 
+        }
+
+        public bool IsPortOpen()
+        {
+            bool _available = false;
+            SerialPort _tempPort;
+            String[] Portname = SerialPort.GetPortNames();
+
+            foreach (string str in Portname)
+            {
+                try
+                {
+                    _tempPort = new SerialPort(str);
+                    _tempPort.Open();
+
+                    if (_tempPort.IsOpen)
+                    {
+                        comboBox2.Items.Add(str);
+                        _tempPort.Close();
+                        _available = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    L("串口枚举失败，因为"+ex.Message,3);
+                    _available = false;
+                }
+            }
+            comboBox2.SelectedItem = 1;
+            if (_available==false) L("没有找到串口设备或其他程序正在使用串口",3);
+            L("串口枚举完成", 1);
+            return _available;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IsPortOpen();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c = '\0';
+            string input = "";
+            if (comboBox2.SelectedItem.ToString() == "请选择")
+            {
+                L("你还没有选择串口",3);
+                return;
+            }
+            if (checkBox1.Checked == false)
+            {
+                if (serialPort1.IsOpen) serialPort1.Close();
+                try
+                {
+                    serialPort1.PortName = comboBox2.SelectedItem.ToString();
+                    if (TrueBAUD == 0)
+                    {
+                        serialPort1.BaudRate = int.Parse(textBox1.Text);
+                    }
+                    else
+                    {
+                        serialPort1.BaudRate = TrueBAUD;
+                        TrueBAUD = 0;
+                    }
+                    serialPort1.DataBits = 8;
+                    serialPort1.Parity = System.IO.Ports.Parity.None;
+                    serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                    serialPort1.ReadTimeout = 1000;
+                    serialPort1.Open();
+                }
+                catch (Exception ex) { L(ex.Message,3); return; }
+                L("串口打开成功",0);
+
+
+                L("发送:AT",1);
+                try{serialPort1.Write("AT");}
+                catch (Exception ex){L(ex.Message,3);return;}
+
+                try
+                {
+                    input = "";
+                    while(true)
+                    {
+                        c = serialPort1.ReadChar();
+                        input = input + ((char)(c));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    L("收到:"+input,2);
+                    if (input == "OK")
+                    {
+                        L("连接正常", 0);
+                    }
+                    else
+                    {
+                        L("连接失败，请检查波特率或选择不知道波特率", 3);
+                        return;
+                    }
+                }
+
+
+                L("发送:AT+NAME"+textBox3.Text,1);
+                try { serialPort1.Write("AT+NAME" + textBox3.Text); }
+                catch (Exception ex) { L(ex.Message,3); return; }
+                try
+                {
+                    input = "";
+                    while (true)
+                    {
+                        c = serialPort1.ReadChar();
+                        input = input + ((char)(c));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    L("收到:" + input,2);
+                    if (input.StartsWith("OKsetname"))
+                    {
+                        L("蓝牙名字设置完成", 0);
+                    }
+                    else
+                    {
+                        L("蓝牙名字设置失败", 3);
+                    }
+                }
+
+
+
+                L("发送:AT+PIN"+textBox4.Text,1);
+                try { serialPort1.Write("AT+PIN" + textBox4.Text); }
+                catch (Exception ex) { L(ex.Message,3); return; }
+                try
+                {
+                    input = "";
+                    while (true)
+                    {
+                        c = serialPort1.ReadChar();
+                        input = input + ((char)(c));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    L("收到:" + input,2);
+                    if (input.StartsWith("OKsetPIN"))
+                    {
+                        L("配对密码设置完成", 0);
+                    }
+                    else
+                    {
+                        L("配对密码设置失败", 3);
+                    }
+                }
+
+
+                try 
+                {
+                    if (comboBox1.SelectedItem.ToString() == "1200"){ string s = "AT+BAUD1"; serialPort1.Write(s); L("发送:"+s,1); }
+                    if (comboBox1.SelectedItem.ToString() == "2400") { string s = "AT+BAUD2"; serialPort1.Write(s); L("发送:" + s,1); }
+                    if (comboBox1.SelectedItem.ToString() == "4800") { string s = "AT+BAUD3"; serialPort1.Write(s); L("发送:" + s,1); }
+                    if (comboBox1.SelectedItem.ToString() == "9600") { string s = "AT+BAUD4"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "19200") { string s = "AT+BAUD5"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "38400") { string s = "AT+BAUD6"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "57600") { string s = "AT+BAUD7"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "115200") { string s = "AT+BAUD8"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "230400") { string s = "AT+BAUD9"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "460800") { string s = "AT+BAUDA"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "921600") { string s = "AT+BAUDB"; serialPort1.Write(s); L("发送:" + s, 1); }
+                    if (comboBox1.SelectedItem.ToString() == "1382400") { string s = "AT+BAUDC"; serialPort1.Write(s); L("发送:" + s, 1); }
+                }
+                catch (Exception ex) { L(ex.Message, 3); return; }
+
+                try
+                {
+                    input = "";
+                    while (true)
+                    {
+                        c = serialPort1.ReadChar();
+                        input = input + ((char)(c));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    L("收到:" + input,2);
+                    if (input.StartsWith("OK"))
+                    {
+                        L("波特率设置完成", 0);
+                    }
+                    else
+                    {
+                        L("波特率设置失败", 3);
+                    }
+                }
+                L("全部操作完成",0);
+            }
+            else
+            {
+                int[] BAUD = new int[]{1200,2400,4800,
+                                        9600,19200,38400,
+                                        57600,115200,230400,
+                                        460800,921600,1382400};
+
+                if (serialPort1.IsOpen) serialPort1.Close();
+                serialPort1.PortName = comboBox2.SelectedItem.ToString();
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = System.IO.Ports.Parity.None;
+                serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                serialPort1.ReadTimeout = 1000;
+
+                int i = 0;
+                for (; i < 12; i++)
+                {
+                    L("尝试"+BAUD[i]+"波特率", 0);
+                    try
+                    {
+                        if (serialPort1.IsOpen) serialPort1.Close();
+                        serialPort1.BaudRate =BAUD[i];
+                        serialPort1.Open();
+                    }
+                    catch (Exception ex) { L(ex.Message, 3); return; }
+
+                    try { serialPort1.Write("AT"); }
+                    catch (Exception ex) { L(ex.Message, 3); return; }
+                    try
+                    {
+                        input = "";
+                        while (true)
+                        {
+                            c = serialPort1.ReadChar();
+                            input = input + ((char)(c));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //L("收到:" + input, 2);
+                        if (input == "OK")
+                        {
+                            L("波特率是" + BAUD[i], 2);
+                            break;
+                        }
+                        else
+                        {
+                            L("波特率不是" + BAUD[i], 3);
+                        }
+                    }
+                }
+
+                if (i == 12)
+                {
+                    L("未能找到这个蓝牙模块的波特率",3);
+                }
+                else
+                {
+                    textBox1.Text = BAUD[i] + "";
+                    checkBox1.Checked = false;
+                    TrueBAUD = BAUD[i];
+                    button1.PerformClick();
+                }
+
+            }
+            serialPort1.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            L("帮助", 0);
+            L("本程序可以用来快速配置蓝牙模块，即使不知道模块的波特率。",0);
+            L("在两个模块没有配对上（指示灯闪烁）时，进行配置。",3);
+            L("只需要将主从模块的密码和波特率配置相同，再次上电即可自动配对。", 0);
+            L("主模块不能配置名字（配置主模块名字会失败）但不影响配对",0);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            L("关于", 1);
+            L("版本:1.0",0);
+            L("作者:James Murray",0);
+            L("源代码:https://github.com/JamesMurrayBIT/BluetoothFastConfig", 0);
+        }
+    }
+
+}
