3c970c0bfb3e999589ba3a9b459343e546248360
JamesMurrayBIT <jmy9605@163.com>
2017-11-19 21:21:08 +0800

    submit

--- BluetoothFastConfig/Form1.cs
+++ BluetoothFastConfig/Form1.cs
@@ -328,11 +328,13 @@ namespace BluetoothFastConfig
         private void button3_Click(object sender, EventArgs e)
         {
             richTextBox1.Text = "";
-            L("帮助", 0);
+            L("蓝牙快速配置助手",1);
             L("本程序可以用来快速配置蓝牙模块，即使不知道模块的波特率。",0);
+            L("", 0);
+            L("帮助：", 1);
             L("在两个模块没有配对上（指示灯闪烁）时，进行配置。",3);
             L("只需要将主从模块的密码和波特率配置相同，再次上电即可自动配对。", 0);
-            L("主模块不能配置名字（配置主模块名字会失败）但不影响配对",0);
+            L("主模块不能配置名字（配置主模块名字会失败）但不影响模块正常使用。",0);
         }
 
         private void button4_Click(object sender, EventArgs e)
